04bb44f6e767919e648bc236dec70e1a6d39c61c
Ari Rojas Céspedes <arojasce@emeal.nttdata.com>
2022-05-19 18:43:21 -0400

    ARC: CR from CRUD's

--- src/app/contador/contador/contador.component.ts
+++ src/app/contador/contador/contador.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contador',
-  templateUrl: './contador.component.html'
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.css']
 })
 export class ContadorComponent {
   numero: number = 0;
